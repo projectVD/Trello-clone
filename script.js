08d83f77bb98219c00a1e4aee4a9a7511faffeba
projectVD <vladdegtarov1998@gmail.com>
2019-12-17 15:52:13 +0200

    z1

---
+++ script.js
@@ -0,0 +1,31 @@
+document
+        .querySelectorAll('.column')
+        .forEach(Column.process)
+
+document 
+        .querySelector('[data-action-addColumn]')
+        .addEventListener('click', function (event){
+            const columnElement = document.createElement('div')
+            columnElement.classList.add('column')
+            columnElement.setAttribute('draggable', 'true')
+            columnElement.setAttribute('data-column-id', Column.idCounter)
+
+            columnElement.innerHTML = 
+                '<p class="column-header">В плане</p>'+
+                '<div data-notes></div>'+
+                '<p class="column-footer">'+
+                '    <span data-action-addNote class="action">+ Добавить карточку</span>'+
+                '</p>'
+
+            Column.idCounter++
+            document.querySelector('.columns').append(columnElement)
+            Column.process(columnElement)
+
+
+        })
+
+document
+        .querySelectorAll('.note')
+        .forEach(Note.process)
+
+
